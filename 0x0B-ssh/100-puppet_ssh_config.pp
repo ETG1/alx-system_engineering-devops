532b7b7b2ccb59a82987cc30e1c6656145f5e75f
ETG1 <elijahkazungu22@gmail.com>
2024-03-19 20:57:28 -0700

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,7 @@ file { '/etc/ssh/ssh_config':
   content => "
     #SSH client configuration
     host
-        HostName 100.25.211.240
+        HostName 54.236.84.73
         User ubuntu
         IdentityFile ~/.ssh/school
         PasswordAuthentication no
